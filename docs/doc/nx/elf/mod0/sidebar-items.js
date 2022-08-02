24f3015c3380bc3d2e8e7ab56b4a71517fc3329e
XorTroll <xabofdez@gmail.com>
2022-08-03 01:48:08 +0200

    Forgot to update docs

---
+++ docs/doc/nx/elf/mod0/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["find_start_dyn_address","Finds the [`Dyn`][`super::Dyn`] reference from a base code address"]],"struct":[["Header","Represents the `MOD0` header structure"],["ModuleStart","Represents the `MOD0` start layout"]]};
