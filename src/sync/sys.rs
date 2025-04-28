02b4997f3291a03db6b430f35fb89c01fa31760c
Philip Woolford <woolford.philip@gmail.com>
2025-04-28 13:38:16 +0930

    Get rid of `mod.rs` to keep consistent.

---
+++ src/sync/sys.rs
@@ -0,0 +1,11 @@
+//use core::arch::asm;
+
+use crate::thread;
+
+pub mod mutex;
+pub mod rwlock;
+
+#[inline(always)]
+fn get_current_thread_handle() -> u32 {
+    unsafe { (*thread::get_thread_local_region()).nx_thread_vars.handle }
+}
