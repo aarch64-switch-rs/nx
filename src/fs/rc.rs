fe223ce59198a63694f0c6cfb7e90604503c31bd
Philip Woolford <woolford.philip@gmail.com>
2026-09-10 15:19:34 +0930

    Remove error type duplicated from standard errors.

--- src/fs/rc.rs
+++ src/fs/rc.rs
@@ -6,7 +6,6 @@ use crate::rc;
 pub const RESULT_SUBMODULE: u32 = 700;
 
 result_define_subgroup!(rc::RESULT_MODULE, RESULT_SUBMODULE => {
-    DeviceNotFound: 1,
-    InvalidPath: 2,
-    NotInSameFileSystem: 3
+    NotInSameFileSystem: 1,
+    DeviceNotFound: 2
 });
